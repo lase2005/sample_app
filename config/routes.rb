ef6c7fde29ba4991c7fee377b69e1ac1641f26f7
lase2005 <brandon@bigeasyconnect.com>
2012-08-21 18:46:44 -0500

    Add authentication

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,7 @@ SampleApp::Application.routes.draw do
 
   root to: 'static_pages#home'
   
-  match '/signup',  to: 'users#new'
-  
+  match '/signup',  to: 'users#new'  
   match '/contact', to: 'static_pages#contact'
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
